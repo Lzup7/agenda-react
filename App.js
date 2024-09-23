75a79fc66c106e767ed14da3b1c7f15ff7c06d1c
Lucas Olvr. <lucasgamerzup@gmail.com>
2024-09-23 20:47:18 -0300

    add
    Add

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
+import { SafeAreaView, StyleSheet, Text, View, Platform, StatusBar} from 'react-native';
+
+import Main from './src/Main/index';
 export default function App() {
+
+
+    /* importando fonts*/
+  const [isFontsLoaded] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+
+    if (!isFontsLoaded){
+      return null;
+    }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <Main>
+      
+    </Main>
+
+  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight: 0,
+    // flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+
 });
+
+
